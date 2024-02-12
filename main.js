47f4430148e0253057e9d36cd5c0458f8e6bed91
Eunju Kang <wealthinkang@gmail.com>
2024-02-12 22:07:00 +0900

    first commit

--- main.js
+++ main.js
@@ -1,89 +1,93 @@
-//랜덤번호 지정
-//유저가 번호를 입력한다. 그리고 go 라는 버튼을 누름
-//만약에 유저가 랜덤번호를 맞추면, 맞췄습니다!
-//랜던 번호가 < 유저번호 Down!!
-//랜던 번호가 > 유저번호 Up!!
-//Rest버튼을 누르면 게임이 리셋된다
-//5번의 기회를 다 쓰면 게임이 끝난다.(더 이상 추측 불가, 버튼이 disable)
-//유저가 1 ~ 100 범위 밖에 숫자를 입력하면 알려준다. + 기회를 깍지 않는다.
-//유저가 이미 입력한 숫자를 또 입력하면 알려준다. + 기회를 깍지 않는다.
 
-let computerNum = 0; //랜덤번호 저장해줄 변수
-let playButton = document.getElementById('play-button');
-let userInput = document.getElementById('user-input');
-let resultArea = document.getElementById('result-area');
-let resetButton = document.getElementById('reset-button');
-let chances = 10;
-let gameOver = false;
-let chanceArea = document.getElementById('chance-area');
+
+let randomNumber = 0 //★★★★★랜덤번호 저장해줄 변수
+let $resultArea = document.getElementById('result-area');
+let $userInput = document.getElementById('user-input'); //input
+let $playButton = document.getElementById('play-button'); //button
+let $chanceArea = document.getElementById('chance-area'); // chance count
+let $resetButton = document.getElementById('reset-button'); //reset button
+let chances  = 5;
 let history = [];
+let gameOver = false;
 
-
-playButton.addEventListener('click', play); //함수도 매개변수로 넘길 수 있다.
-resetButton.addEventListener('click', reset);
-userInput.addEventListener('focus', function() {
-	userInput.value = '';
-})
+$playButton.addEventListener('click', playButton); //Go버튼 클릭할 경우
+$userInput.addEventListener('focus', function() {
+	$userInput.value = '';
+}); //input에 focus할 경우
+$resetButton.addEventListener('click', reset); //reset버튼 클릭할 경우
 
 function pickRandomNum(){
-	//1부터 100까지 랜덤숫자 뽑기 
-	//Math.random()은 0부터 1까지 랜덤하게 숫자 출력 But!! 1은 포함이 안돼는 1에 가까운 숫자를 반환
-	//0 ~ 99까지 출력되는데 1 ~ 100까지 출력하기 위해 +1 을 해준다.
-	computerNum = Math.floor(Math.random() * 10) + 1;
-	console.log('정답', computerNum);
+	randomNumber = Math.floor(Math.random() * 10) + 1; //컴퓨터 랜덤 숫자 1 ~ 10
+	console.log(randomNumber);
 }
 
-function play() {
-	let userValue = userInput.value;
+function playButton(){
+	inputValue = parseInt($userInput.value); //input value
+	console.log(inputValue);
 
-	if(userValue < 1 || userValue > 10) {
-		resultArea.textContent = '1과 10사이 숫자를 입력해 주세요';
-		return; //해당 블록에서 실행을 끝낸다.
+	if(inputValue < 1 || inputValue > 10) {
+		$resultArea.textContent = '1 ~ 10까지 숫자를 입력하세요!!'
+		return
 	}
 
-	if(history.includes(userValue)) {
-		resultArea.textContent = '이미 입력한 숫자입니다 다른 숫자를 입력해 주세요';
+	if(history.includes(inputValue)) {
+		$resultArea.textContent = '이미 입력한 숫자입니다 다른 숫자를 입력해 주세요';
 		return;
 	}
-
-	chances -- ;
-	chanceArea.textContent = `남은기회: ${chances}번`;
-	console.log('chances', chances);
 	
-	if(userValue < computerNum) {
-		resultArea.textContent = 'Up!!!';
-	} else if (userValue > computerNum) {
-		resultArea.textContent = 'Down!!!';
-	} else {
-		resultArea.textContent = '맞췄습니다!!!';
-		gameOver = true;
+	if(randomNumber !== inputValue){
+		chances -- ;
+		$chanceArea.textContent = `남은기회 ${chances}번`;
+		console.log('chanceArea' + chances);
 	}
-	
-	history.push(userValue)
-	console.log(history);
+
+	if(randomNumber < inputValue){
+		$resultArea.textContent ='Down!!';
+	} else if(randomNumber > inputValue) {
+		$resultArea.textContent ='up!!';
+	} else if(randomNumber === inputValue){
+		$resultArea.textContent = '아이구 맞췄네!!'
+		//play버튼 활성화
+		$playButton.disabled = true;
+	};
+
+	history.push(inputValue);
 
 	if(chances < 1) {
-		gameOver = true;
-	}
-
-	if(gameOver) {
-		playButton.disabled = true;
+		$resultArea.textContent = '안타깝네요! 다시 도전 하시겠습니까?'
+		$playButton.disabled = true;
 	}
 }
 
-//리셋
 function reset() {
 	//user input창이 깨끗하게 정리되고
-	userInput.value = '';
+	$userInput.value = '';
+	//play버튼 활성화
+	$playButton.disabled = false;
 	//새로운 번호가 생성되고
+	//히스토리 리셋
+	history = [];
+	//랜덤번호 리셋
 	pickRandomNum();
-	resultArea.textContent = '결과값이 여기 나옵니다.'
+	//결과값 리셋	
+	$resultArea.textContent = '준비하시고 시작!!'
+	//찬스 카운트 리셋
+	chances = 5;
+	//남은기회 리셋
+	$chanceArea.innerHTML = `남은 기회:${chances}`;
+}
 
-	gameOver = false;
-	playButton.disabled = false;
-	chances = 10;
-	chanceArea.innerHTML = `남은 기회:${chances}`;
-	userValueList = [];
+const $animateNumber = document.getElementById('animate-number');
+
+function animateNumber() {
+	setInterval(() => {
+    for (let i = 1; i <= 10; i++) {
+      setTimeout(() => {
+        $animateNumber.textContent = i;
+      }, (i - 1) * 600); //1초마다 숫자 변경
+    }
+  }, 1000); // 10초마다 애니메이션 반복
 }
 
 pickRandomNum();
+animateNumber();
